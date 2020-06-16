df3150ad01f3022c92b7a7936a67ba571155ad38
D <danixoon@gmail.com>
2020-06-17 00:38:51 +0300

    php routing

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
 <?php
+require_once realpath(__DIR__ . "/config.php");
+require_once realpath(__DIR__ . "/template/header.php");
+require_once realpath(__DIR__ . "/template/menu.php");
 
-require_once("page/main.php");
+render_header("Главная");
+
+?>
+
+<body>
+  <?php
+  $menu_items = array();
+  foreach ($PAGE_SCHEMA as $key => $value) {
+    array_push($menu_items, array("name" => $value["name"], "link" => "/index.php?page={$key}", "selected" => $PAGE === $key));
+  }
+  render_menu($menu_items);
+  ?>
+  <!--   ------------------------------------------------------------------>
+  <div class="content">
+    <div class="header shadow">
+      <div class="img-header"></div>
+      <h1><?php echo $PAGE_SCHEMA[$PAGE]["name"] ?></h1>
+    </div>
+    <div class="plus shadow">
+      <div class="header-plus blue">
+        <a href="Profile.html" class="button" style="color: white;">Войдите</a>
+      </div>
+      <div class="content-plus">
+      </div>
+
+    </div>
+    <p class="por">или</p>
+
+    <div class="plus shadow">
+      <div class="header-plus red">
+        <a href="Profile.html" class="button" style="color: white;">Зарегистрируйтесь</a>
+      </div>
+      <div class="content-plus">
+
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ template/header.php
@@ -0,0 +1,17 @@
+<?php
+
+function render_header($title)
+{
+?>
+  <title><?php echo $title ?></title>
+  <link rel="stylesheet" href="/public/style.css">
+  <meta charset="UTF-8">
+  <meta name=viewport content="width=device-width, initial-scale=1.0 ">
+  <meta name="description" content="**>
+<link rel=" shortcut icon" href="EMC.svg" type="image/x-icon">
+  <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic|Open+Sans:300,regular,italic,700&subset=latin,latin-ext,cyrillic,hebrew" rel="stylesheet">
+<?php
+}
+
+?>
