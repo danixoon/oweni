844dc3ce203406c1c20d8b525137e121fce54935
DeliriumtheD <50268563+DeliriumtheD@users.noreply.github.com>
2020-06-26 11:04:46 +0300

    sql

--- template/task.php
+++ template/task.php
@@ -149,11 +149,11 @@ function render_task_list()
         <div class="modal__content" style="  text-align: left;padding: 2%;">
           <form action="/api/remove.php" onsubmit="return onFormSubmit(this)">
             <label>Истиность</label>
-            <input type="text" name="truethy" value="<?php echo $profile_data["truethy"];?>">
+            <input type="text" name="truethy" value="<?php echo $profile_data["truethy"]; ?>">
             <label>Индекс НПУ</label>
-            <input type="text" name="score" value="<?php echo $profile_data["score"];?>">
+            <input type="text" name="score" value="<?php echo $profile_data["score"]; ?>">
             <label>ФИО заполнившего</label>
-            <input type="text" name="name" value="<?php echo $profile_data["name"];?>">
+            <input type="text" name="name" value="<?php echo $profile_data["name"]; ?>">
             <br>
             <input type="hidden" name="id" value="<?php echo $profile_id; ?>">
             <input type="submit" value="Сохранить">
@@ -177,20 +177,21 @@ function render_task_list()
       <?php render_modal($row, $profile_id); ?>
 
       <select onchange="switch(this.value) {
-          // case 'edit':
-          //    toggleCardEditModal(<?php echo $profile_id; ?>);
-          //    break;
-          // case 'delete':
-          //    toggleCardRemoveModal(<?php echo $profile_id; ?>);
-          //    break;
-          case 'edit':
-            toggleFormModal(<?php echo $profile_id; ?>);
-            break;
+            case 'edit':
+              toggleCardEditModal(<?php echo $profile_id; ?>);
+              break;
+            case 'delete':
+              toggleCardRemoveModal(<?php echo $profile_id; ?>);
+              break;
+              case 'testResult':
+                toggleFormModal(<?php echo $profile_id; ?>);
+                break;
           }
           " style="width:20px; float:right">
         <option disabled selected value=""></option>
+        <option value="testResult">Результаты Теста</option>
         <option value="edit">Изменить</option>
-        <!-- <option value="delete">Удалить</option> -->
+        <option value="delete">Удалить</option>
       </select>
 
       <p style="text-align:left" class="text">
